91297e501829eeae145a888161e302b9b621591d
Renata Rakhmanina <renatarahmanina@gmail.com>
2024-03-20 23:57:44 +0100

    Initial commit

---
+++ src/js/_tests_/basic.test.js
@@ -0,0 +1,17 @@
+import sortHeroesByHealth from "../basic";
+
+test('sorts heroes by health', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortedHeroes = sortHeroesByHealth(heroes);
+
+  expect(sortedHeroes).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
